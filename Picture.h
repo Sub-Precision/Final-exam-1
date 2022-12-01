597797b00020726de26538969fb250236b8ff09b
Sub-Precision <119545954+Sub-Precision@users.noreply.github.com>
2022-11-30 17:06:51 -0700

    Update Picture.h

--- Picture.h
+++ Picture.h
@@ -9,6 +9,7 @@ using namespace std;
 
 class Picture {
 public:
+    //Picture private variables
     string name;
     int year;
     int nominations;
@@ -17,9 +18,10 @@ public:
     string genre1;
     string genre2;
     int release;
-    int metacrtic;
+    int metacritic;
     string synopsis;
 
+    //Getters
     int getName() const;
     int getYear() const;
     int getNominations() const;
@@ -28,8 +30,20 @@ public:
     string getGenre1() const;
     string getGenre2() const;
     int getRelease() const;
-    int getMetacrtic() const;
+    int getMetacritic() const;
     string getSynopsis() const;
+    
+    //Setters
+    void setName(string na);
+    void setYear(int ye);
+    void setNominations(int no);
+    void setRating(double ra);
+    void setDuration(int du);
+    void setGenre1(int one);
+    void setGenre2(int two);
+    void setRelease(int re);
+    void setMetacritic(int me);
+    void setSynopsis(string sy);
 };
 
 
