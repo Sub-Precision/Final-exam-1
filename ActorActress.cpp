a2c70d7194cf007591085e8576ebb2e5e4094340
Sub-Precision <119545954+Sub-Precision@users.noreply.github.com>
2022-11-30 17:23:11 -0700

    Update ActorActress.cpp

--- ActorActress.cpp
+++ ActorActress.cpp
@@ -5,22 +5,44 @@
 #include "ActorActress.h"
 using namespace std;
 
+// Contructor functionality
+ActorActress::ActorActress()
+{
+}
+ActorActress::ActorActress(int, string, bool, string, string)
+{
+}
+    
+//Implemented Getters
 int ActorActress::getYear() const {
     return year;
 }
-
 string ActorActress::getAward() const {
     return award;
 }
-
 bool ActorActress::getWinner() const {
     return winner;
 }
-
 string ActorActress::getName() const {
     return name;
 }
-
 string ActorActress::getFilm() const {
     return film;
+}
+
+//Implemented Setters
+void ActorActress::setYear(int y) {
+    year = y;
+}
+void ActorActress::setAward(int a) {
+    award = a;
+}
+void ActorActress::setWinner(int w) {
+    winner = w;
+}
+void ActorActress::setName(int n) {
+    name = n;
+}
+void ActorActress::setFilm(int f) {
+    film = f;
 }
